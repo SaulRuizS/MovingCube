1c4a86015d8dad3649d2ce3b3a90d93a9499c5b9
Saul Ruiz <saul.f.ruiz.silva@gmail.com>
2022-08-11 19:06:59 -0500

    Main elements addition

---
+++ index.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+
+const canvas = document.createElement('canvas');
+
+body.appendChild(canvas);
+
+// alert(canvas.height);
+console.log(canvas.height);
+console.log(canvas.width);
