16aff1a2f40699deef7cd8cd73b38af0fe692b02
Saul Ruiz <saul.f.ruiz.silva@gmail.com>
2022-08-19 21:15:28 -0500

    Strugling with webgl

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// import { mat4 } from 'glMatrix';
+
 const body = document.querySelector('body');
 
 const canvas = document.createElement('canvas');
@@ -37,8 +39,8 @@ canvas.width = 800;
 const vsSource = `
     attribute vec4 attributeVertexPos;
 
-    uniform mat4 uniformModelViewMatrix;
     uniform mat4 uniformProjectionMatrix;
+    uniform mat4 uniformModelViewMatrix;
 
     void main() {
         gl_Position = uniformProjectionMatrix * uniformModelViewMatrix * attributeVertexPos;
@@ -73,7 +75,99 @@ const initShader = (ctx, vShaderSource, fShaderSource) => {
     return shaderProgram;
 }
 
+const initBuffers = (ctx) => {
+    const positionBuffer = ctx.createBuffer();
 
+    ctx.bindBuffer(ctx.ARRAY_BUFFER, positionBuffer);
+
+    const positions = [
+        1.0, 1.0,
+        -1.0, 1.0,
+        1.0, -1.0,
+        -1.0, -1.0,
+    ];
+
+    ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(positions), ctx.STATIC_DRAW);
+
+    return {
+        position: positionBuffer,
+    };
+}
+
+const drawScene = (ctx, programInfo, buffers) => {
+    ctx.clearColor(0.3, 0.8, 0.0, 1.0);
+    ctx.clearDepth(1.0);
+    ctx.enable(ctx.DEPTH_TEST);
+    ctx.depthFunc(ctx.LEQUAL);
+    ctx.clear(ctx.COLOR_BUFFER_BIT | ctx.DEPTH_BUFFER_BIT);
+
+    const fieldOfView = 45 * Math.PI / 180;
+
+    const aspect = ctx.canvas.width / ctx.canvas.height;
+
+    const zNear = 0.1;
+
+    const zFar = 100.0;
+
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(
+            projectionMatrix,
+            fieldOfView,
+            aspect,
+            zNear,
+            zFar
+        )
+
+    const modelViewMatrix = mat4.create();
+
+    mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
+
+    const numComponents = 2;
+
+    const type = ctx.FLOAT;
+
+    const normalize = false;
+
+    const stride = 0;
+    
+    const offset = 0;
+
+    ctx.bindBuffer(ctx.ARRAY_BUFFER, buffers.position);
+    
+    ctx.vertexAtribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset,
+    );
+    
+    ctx.enableVertexAttribArray(
+        programInfo.attribLocations.vertexPosition
+    );
+    
+    ctx.useProgram(programInfo.program)
+
+    ctx.uniformMatrix4fv(
+        programInfo.uniformLocations.projectionMatrix,
+        false,
+        projectionMatrix
+    );
+
+    ctx.uniformMatrix4fv(
+        programInfo.uniformLocations.modelViewMatrix,
+        false,
+        modelViewMatrix
+    );
+
+    {
+        const offset = 0;
+        const vertexCount = 4;
+        ctx.drawArrays(ctx.TRIANLGE_STRIP, offset, vertexCount);
+    }
+}
 
 const main = () => {
     let ctxWebl = canvas.getContext('webgl');
@@ -81,10 +175,25 @@ const main = () => {
     if(ctxWebl === null) {
         alert('Unable to initialize WebGL. Your browser or machine may not support it.');
     }
-    ctxWebl.clearColor(0.3, 0.9, 0.1, 1.0);
-    ctxWebl.clear(ctxWebl.COLOR_BUFFER_BIT);
+    // ctxWebl.clearColor(0.3, 0.9, 0.1, 1.0);
+    // ctxWebl.clear(ctxWebl.COLOR_BUFFER_BIT | ctxWebl.DEPTH_BUFFER_BIT);
 
-    // const shaderProgram = initShader(ctxWebl);
+    const shaderProgram = initShader(ctxWebl, vsSource, fsSource);
+
+    const programInfo = {
+        program: shaderProgram,
+        attribLocations: {
+            vertexPosition: ctxWebl.getAttribLocation(shaderProgram, 'attributeVertexPos'),
+        },
+        uniformLocations: {
+            projectionMatrix: ctxWebl.getUniformLocation(shaderProgram,'uniformProjectionMatrix'),
+            modelViewMatrix: ctxWebl.getUniformLocation(shaderProgram, 'uniformModelViewMatrix'),
+        },
+    }
+
+    const buffers = initBuffers(ctxWebl);
+
+    drawScene(ctxWebl, programInfo, buffers);
 }
 
 window.onload = main;
