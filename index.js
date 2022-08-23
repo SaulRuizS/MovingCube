6f21e69da4a88e7f8b39237fc60e683d2867400e
Saul Ruiz <saul.f.ruiz.silva@gmail.com>
2022-08-23 18:36:31 -0500

    Basic lines added

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(
-    75, //FOV
-    window.innerWidth / innerHeight, //Aspect Ratio
-    0.1, //Near
-    1000 //Far
-);
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(
     window.innerWidth,
@@ -15,23 +6,48 @@ renderer.setSize(
 
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const scene = new THREE.Scene();
 
+const camera = new THREE.PerspectiveCamera(
+    45, //FOV
+    window.innerWidth / innerHeight, //Aspect Ratio
+    1, //Near
+    500 //Far
+);
+
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+
+
+//Cube
+const geometry = new THREE.BoxGeometry(10, 10, 10);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+//
 
-// camera.position.x = 1;
-// camera.position.y = 1;
-camera.position.z = 2;
+//Triangle
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
+const points = [];
+points.push( new THREE.Vector3(-10, 0, 0) );
+points.push( new THREE.Vector3(0, 10, 0) );
+points.push( new THREE.Vector3(10, 0, 0) );
+const triangleGeometry = new THREE.BufferGeometry().setFromPoints(points);
+const line = new THREE.Line(triangleGeometry, lineMaterial);
+//
 
-// cube.rotation.y = 1
-
-const animate = () => {
-    requestAnimationFrame( animate );
+const animateCube = () => {
+    requestAnimationFrame( animateCube );
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 };
 
-animate();
+const animateTriangle = () => {
+    scene.add(line);
+    renderer.render(scene, camera);
+}
+
+// animateTriangle();
+
+animateCube();
